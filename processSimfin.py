374475e721a0f0e4a631d2a15a6b8996b106bebc
Ran <ran@Rans-Home-MacBook-Pro.local>
2017-11-19 23:06:51 +0200

    Adding minimum Year filter

--- processSimfin.py
+++ processSimfin.py
@@ -2,6 +2,7 @@
 
 import extractor
 import getopt,sys,os
+import re
 try:
     import xlsxwriter
 except ImportError:
@@ -14,7 +15,7 @@ def nextCol(max):
         yield col_num
         col_num +=1
 
-def parseDb(input_file_name):
+def parseDb(input_file_name,minYear):
     dataset = extractor.SimFinDataset(input_file_name)
     
     file_name = os.path.splitext(input_file_name)[0] # remove extension
@@ -34,6 +35,11 @@ def parseDb(input_file_name):
     
     
     worksheet.write(0, next(col_gen), "Period Name")
+    worksheet.write(0, next(col_gen), "Report Type")
+    worksheet.write(0, next(col_gen), "Report Seq")
+    worksheet.write(0, next(col_gen), "Report Year")
+    period_name_pattern = "(\w)(\d)-(\d{4})"
+    period_name_re = re.compile(period_name_pattern)
     
     numMissingIndicators = 0
     num_columns = next(col_gen) - 1
@@ -42,21 +48,38 @@ def parseDb(input_file_name):
     row = 1
     for companyIdx,company in enumerate(dataset.companies):
         for periodIdx,time_period in enumerate(dataset.timePeriods):
-            col_gen = nextCol(100)
-            worksheet.write(row, next(col_gen), company.name)
-            worksheet.write(row, next(col_gen), company.ticker)
-            for indIdx,indicator in enumerate(company.data):
-                if indicator.name != indicator_name_list[indIdx]:
-                    print "%s in not in initial list for ticker %s" % (indicator.name,company.ticker)
-                    numMissingIndicators += 1
-                    worksheet.write(row, next(col_gen), "NA")
-                else:
-                    if (indicator.values[periodIdx] == None):
+            result = period_name_re.match(time_period)
+            if (result):
+                type = result.group(1)
+                seqNum = int(result.group(2))
+                year = int(result.group(3))
+            else:
+                type = "NA"
+                seqNum = "NA"
+                year = "NA"
+                
+            if (year > minYear): # ignore
+                col_gen = nextCol(100)
+                worksheet.write(row, next(col_gen), company.name)
+                worksheet.write(row, next(col_gen), company.ticker)
+                for indIdx,indicator in enumerate(company.data):
+                    if indicator.name != indicator_name_list[indIdx]:
+                        print "%s in not in initial list for ticker %s" % (indicator.name,company.ticker)
+                        numMissingIndicators += 1
                         worksheet.write(row, next(col_gen), "NA")
                     else:
-                        worksheet.write(row, next(col_gen), indicator.values[periodIdx])
-            worksheet.write(row, next(col_gen), time_period)
-            row += 1
+                        if (indicator.values[periodIdx] == None):
+                            worksheet.write(row, next(col_gen), "NA")
+                        else:
+                            worksheet.write(row, next(col_gen), indicator.values[periodIdx])
+                worksheet.write(row, next(col_gen), time_period)
+                worksheet.write(row, next(col_gen), type)
+                worksheet.write(row, next(col_gen), seqNum)
+                worksheet.write(row, next(col_gen), year)
+            
+                row += 1
+            else:
+               pass
         print "Written Company %d/%d (%d%%)" % (companyIdx,numCompanies,100*companyIdx/numCompanies)
 
 
@@ -79,8 +102,9 @@ def print_usage():
 
 def main():
     input_file_name = None
+    minYear = 0
     try:
-        opts, args = getopt.getopt(sys.argv[1:] ,'', ["help","inputFile="])
+        opts, args = getopt.getopt(sys.argv[1:] ,'', ["help","inputFile=","minYear="])
     except getopt.GetoptError as err:
         # print help information and exit:
         print str(err)  # will print something like "option -a not recognized"
@@ -93,6 +117,10 @@ def main():
         elif opt in ("--inputFile"):
             input_file_name = val
             print "Will read from %s" % input_file_name
+        elif opt in ("--minYear"):
+            minYear = int(val)
+            print "Will ignore reports from years before %u" % minYear
+
         else:
             assert False, "unknown option %s" % opt 
 
@@ -103,7 +131,7 @@ def main():
         else:
             print "%s doesn't exist" % input_file_name
             return
-        parseDb(input_file_name)
+        parseDb(input_file_name,minYear)
     else:
         print "No input file name given!"
         print_usage()
